c1b3108a7ad28b914603b718965f7599d6823eec
Sunghyun Cho <mail@chosunghyun.com>
2022-12-19 12:30:39 +0900

    Initial Commit

---
+++ sky-earth-human/sky_earth_humanApp.swift
@@ -0,0 +1,20 @@
+//
+//  sky_earth_humanApp.swift
+//  sky-earth-human
+//
+//  Created by Sunghyun Cho on 12/19/22.
+//
+
+import SwiftUI
+
+@main
+struct sky_earth_humanApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
