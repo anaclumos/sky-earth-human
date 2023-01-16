7afad4ad7fa9e04964484cbd2bcfed7b1fc253d7
Sunghyun Cho <mail@chosunghyun.com>
2023-01-15 19:27:21 -0800

    add basic install guide

--- sky-earth-human/ContentView.swift
+++ sky-earth-human/ContentView.swift
@@ -9,81 +9,42 @@ import CoreData
 import SwiftUI
 
 struct ContentView: View {
-  @Environment(\.managedObjectContext) private var viewContext
-
-  @FetchRequest(
-    sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-    animation: .default
-  )
-  private var items: FetchedResults<Item>
-
   var body: some View {
-    NavigationView {
-      List {
-        ForEach(items) { item in
-          NavigationLink {
-            Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-          } label: {
-            Text(item.timestamp!, formatter: itemFormatter)
-          }
-        }
-        .onDelete(perform: deleteItems)
+    ScrollView {
+      VStack(alignment: .leading) {
+        Text((Bundle.main.infoDictionary?["CFBundleDisplayName"] as? String)!)
+          .font(.title).padding(5)
+        HStack {
+          Text("버전 " + (Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String)!)
+          Spacer()
+          Text("LA, 서울, 그리고 대전")
+        }.padding(5)
+          .font(.subheadline)
+          .foregroundColor(.secondary)
+        Divider()
+        Text("설치 방법")
+          .font(.title2).padding(5)
+        Text(
+          """
+          1. **설정** 앱 열기
+          2. **일반** 탭으로 이동
+          3. **키보드** 탭으로 이동
+          4. **키보드** 탭으로 다시 한 번 이동
+          5. **새로운 키보드 추가...** 버튼 누르기
+          6. 타사 키보드 중 **\((Bundle.main.infoDictionary?["CFBundleDisplayName"] as? String)!)** 선택!
+          """
+        )
+        .font(.body).padding(5)
       }
-      .toolbar {
-        ToolbarItem(placement: .navigationBarTrailing) {
-          EditButton()
-        }
-        ToolbarItem {
-          Button(action: addItem) {
-            Label("Add Item", systemImage: "plus")
-          }
-        }
-      }
-      Text("Select an item")
+      .padding()
     }
-  }
-
-  private func addItem() {
-    withAnimation {
-      let newItem = Item(context: viewContext)
-      newItem.timestamp = Date()
-
-      do {
-        try viewContext.save()
-      } catch {
-        // Replace this implementation with code to handle the error appropriately.
-        // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-        let nsError = error as NSError
-        fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-      }
-    }
-  }
-
-  private func deleteItems(offsets: IndexSet) {
-    withAnimation {
-      offsets.map { items[$0] }.forEach(viewContext.delete)
-
-      do {
-        try viewContext.save()
-      } catch {
-        // Replace this implementation with code to handle the error appropriately.
-        // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-        let nsError = error as NSError
-        fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-      }
-    }
+    .navigationTitle("하늘땅사람")
+    .navigationBarTitleDisplayMode(.inline)
   }
 }
 
-private let itemFormatter: DateFormatter = {
-  let formatter = DateFormatter()
-  formatter.dateStyle = .short
-  formatter.timeStyle = .medium
-  return formatter
-}()
-
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
-    ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+    ContentView()
   }
 }
--- sky-earth-human/sky_earth_humanApp.swift
+++ sky-earth-human/sky_earth_humanApp.swift
@@ -9,12 +9,9 @@ import SwiftUI
 
 @main
 struct sky_earth_humanApp: App {
-  let persistenceController = PersistenceController.shared
-
   var body: some Scene {
     WindowGroup {
       ContentView()
-        .environment(\.managedObjectContext, persistenceController.container.viewContext)
     }
   }
 }
